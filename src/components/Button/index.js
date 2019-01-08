b88840cc2caa3f26302190d07762e0bd75614b93
Tung Teng <tung.nguyen@employmenthero.com>
2019-01-09 00:08:58 +0700

    Update Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {SVG, Rectangle} from '../../core/ReactRough';
 
 const Button = ({children, color}) => (
-  <SVG>
-    <Rectangle x={120} y={15} width={80} height={40} options={{fill: 'red'}} />
+  <SVG width={80} height={40}>
+    <Rectangle x={0} y={0} width={80} height={40} options={{fill: 'red'}} />
   </SVG>
 );
 
