90c77b06393f6c4c5c843b6fcb8f810c013e620a
Tung Teng <tung.nguyen@employmenthero.com>
2019-01-10 00:24:10 +0700

    Update Input component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
 import {SVG, Rectangle} from '../../core/ReactRough';
 
-const Input = ({children, color}) => (
-  <>
-    <input />
-    <SVG width={90} height={50} style={{cursor: 'pointer'}}>
-      <Rectangle x={5} y={5} width={80} height={40} options={{fill: 'navy'}} />
+const InputWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+  height: 80px;
+  width: 370px;
+`;
+
+const ClearedStyleInput = styled.input`
+  position: relative;
+  background: none;
+  border: none;
+  outline: none;
+  z-index: 1;
+  width: 94.6%;
+  left: 2.7%;
+  height: 75%;
+  top: 12.5%;
+  font-size: 1.5em;
+`;
+
+const Input = props => (
+  <InputWrapper>
+    <ClearedStyleInput {...props} />
+    <SVG
+      width="100%"
+      height="100%"
+      style={{
+        position: 'absolute',
+        left: 0,
+        top: 0,
+      }}>
+      <Rectangle x={5} y={5} width={100} height={70} options={{}} />
     </SVG>
-  </>
+  </InputWrapper>
 );
 
 export default Input;
