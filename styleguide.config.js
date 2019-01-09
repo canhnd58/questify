4825946e7a588e15eada44f9c661081a7d4dbcdc
Tung Teng <tung.nguyen@employmenthero.com>
2019-01-10 01:01:30 +0700

    Update config to not apply Indie Flower font for Code mirror

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,5 +2,16 @@ const path = require('path');
 
 module.exports = {
   webpackConfig: require('./node_modules/react-scripts/config/webpack.config.js'),
-  require: [path.join(__dirname, './src/index.css')],
+  styles: {
+    StyleGuide: {
+      '@global *': {
+        fontFamily: '"Indie Flower", cursive',
+      },
+      '@global .CodeMirror *': {
+        fontFamily:
+          'source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace',
+      },
+    },
+  },
+  exampleMode: 'expand',
 };
