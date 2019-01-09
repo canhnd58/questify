e0d97b39474c58d95ab4d88aa181cd2e3ce9a52f
Tung Teng <tung.nguyen@employmenthero.com>
2019-01-10 02:25:56 +0700

    Fetch font went go to docs page

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   webpackConfig: require('./node_modules/react-scripts/config/webpack.config.js'),
+  require: [path.join(__dirname, './src/index.css')],
   styles: {
     StyleGuide: {
       '@global *': {
