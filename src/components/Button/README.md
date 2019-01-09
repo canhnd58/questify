b417f3d41a3a1ee692d3cf0dff608f106f2c77e6
Canh Nguyen Duc <duccanh9511@gmail.com>
2019-01-10 02:23:18 +0700

    Update Button to show text

--- src/components/Button/README.md
+++ src/components/Button/README.md
@@ -1,3 +1,7 @@
 ```js
-<Button color="pink">Click me</Button>
+<Button color="#cbffbb">Click me</Button>
+<Button color="#f6b2bb">Click me</Button>
+<Button color="#c1ebff">Click me</Button>
+<Button color="#faffa8">Click me</Button>
+<Button color="#ffc78f">Click me</Button>
 ```
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import {SVG, Rectangle} from '../../core/ReactRough';
+import {SVG, Text, Rectangle} from '../../core/ReactRough';
 
-const Button = ({children, color}) => (
-  <SVG width={90} height={50} style={{cursor: 'pointer'}}>
-    <Rectangle x={5} y={5} width={80} height={40} options={{fill: 'red'}} />
-  </SVG>
-);
+class Button extends React.Component {
+  render() {
+    const WIDTH = 80;
+    const HEIGHT = 40;
+    const PADDING = 3;
+    const {children, color} = this.props;
+
+    return (
+      <SVG width={WIDTH} height={HEIGHT} style={{cursor: 'pointer'}}>
+        <Rectangle
+          x={PADDING}
+          y={PADDING}
+          width={WIDTH-2*PADDING}
+          height={HEIGHT-2*PADDING}
+          options={{
+            fill: color,
+            fillWeight: 3,
+          }}
+        />
+        <Text
+          x={WIDTH/2}
+          y={HEIGHT/2}
+          textAnchor={'middle'}
+          alignmentBaseline={'central'}
+        >
+          {children}
+        </Text>
+
+      </SVG>
+    );
+  }
+}
 
 export default Button;
