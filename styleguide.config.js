0a3bf2c41e4fc5619489d8f770450f2d33dbcfdd
Tung Teng <tung.nguyen@employmenthero.com>
2019-01-10 00:10:07 +0700

    Config for using the right font on examples

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+
 module.exports = {
   webpackConfig: require('./node_modules/react-scripts/config/webpack.config.js'),
+  require: [path.join(__dirname, './src/index.css')],
 };
