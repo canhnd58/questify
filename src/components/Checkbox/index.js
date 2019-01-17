d9937e1d437c4a946aade424d11d7474cc638b42
Tung Teng <tung.nguyen@employmenthero.com>
2019-01-17 12:42:32 +0700

    Implement Checkbox component

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {compose, setDisplayName, withState, withHandlers} from 'recompose';
+import {SVG, Rectangle, Path} from '../../core/ReactRough';
+
+const Checkbox = ({checked, toggleChecked}) => (
+  <div style={{display: 'inline-block'}} onClick={toggleChecked}>
+    {checked && (
+      <SVG width={28} height={28}>
+        <Path d="M22 22h-19v-19h19v2h-1v-1h-17v17h17v-9.502h1v10.502zm5-19.315l-14.966 15.872-5.558-6.557.762-.648 4.833 5.707 14.201-15.059.728.685z" />
+      </SVG>
+    )}
+    {!checked && (
+      <SVG width={28} height={28}>
+        <Path d="M22 22h-19v-19h19v2h-1v-1h-17v17h17v-17h1v17z" />
+      </SVG>
+    )}
+  </div>
+);
+
+const enhance = compose(
+  setDisplayName('Checkbox'),
+  withState(
+    'checked',
+    'setChecked',
+    ({defaultChecked}) => defaultChecked || false,
+  ),
+  withHandlers({
+    toggleChecked: ({setChecked}) => () => setChecked(checked => !checked),
+  }),
+);
+
+export default enhance(Checkbox);
