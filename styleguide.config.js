3a09bd5006641839e7bfc1b4bda0304ebe32f0b3
Tung Teng <tung.nguyen@employmenthero.com>
2019-01-23 23:41:35 +0700

    Change styleguide port

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,4 +4,5 @@ module.exports = {
   webpackConfig: require('./node_modules/react-scripts/config/webpack.config.js'),
   require: [path.join(__dirname, './src/index.css')],
   exampleMode: 'expand',
+  serverPort: 8080,
 };
