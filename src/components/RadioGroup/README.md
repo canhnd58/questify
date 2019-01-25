a4d936cb33ec75a77891c79bd3cf773e988fec8f
Tung Teng <tung.nguyen@employmenthero.com>
2019-01-26 01:35:01 +0700

    Add RadioGroup

---
+++ src/components/RadioGroup/README.md
@@ -0,0 +1,11 @@
+##### Default
+
+```js
+<RadioGroup options={['Tung', 'Teng', 'Something']} />
+```
+
+##### Inline
+
+```js
+<RadioGroup options={['Tung', 'Teng', 'Something']} inline />
+```
---
+++ src/components/RadioGroup/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import {compose, setDisplayName, withState, withHandlers} from 'recompose';
+import RadioButton from '../RadioButton';
+import {SVG, Circle} from '../../core/ReactRough';
+
+const Wrapper = styled.div`
+  display: ${props => (props.inline ? 'inline-flex' : 'flex')};
+  align-items: center;
+  cursor: pointer;
+  margin-right: 10px;
+`;
+
+const RadioGroup = ({options = [], activeRadio, setActiveRadio, inline}) =>
+  options.map((value, key) => (
+    <Wrapper key={key} onClick={() => setActiveRadio(key)} inline={inline}>
+      <RadioButton key={key} active={activeRadio === key} />
+      {value}
+    </Wrapper>
+  ));
+
+const enhance = compose(withState('activeRadio', 'setActiveRadio', null));
+
+export default enhance(RadioGroup);
