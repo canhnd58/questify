6cbeaa19df7f0c58637eb92a899ed59750c915be
Tung Teng <tung.nguyen@employmenthero.com>
2019-02-17 12:02:40 +0700

    Update TriangleButton: Add children prop

--- src/components/TriangleButton/README.md
+++ src/components/TriangleButton/README.md
@@ -1,6 +1,18 @@
+##### Default
+
 ```js
 <TriangleButton width="40" height="60" direction="left"/>
 <TriangleButton width="40" height="60" direction="right"/>
 <TriangleButton width="60" height="40" direction="up"/>
 <TriangleButton width="60" height="40" direction="down"/>
 ```
+
+##### With children
+
+```js
+<TriangleButton width="40" height="60" direction="left" style={{marginRight: 10}}>Left</TriangleButton>
+<TriangleButton width="40" height="60" direction="right">Right</TriangleButton>
+<TriangleButton width="60" height="40" direction="up">Up</TriangleButton>
+<TriangleButton width="60" height="40" direction="down">Down</TriangleButton>
+```
+
--- src/components/TriangleButton/index.js
+++ src/components/TriangleButton/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 import {SVG, Polygon} from '../../core/ReactRough';
 
-const ButtonSVG = styled(SVG)`
+const FLEX_DIRECTION_MAP = {
+  left: 'row',
+  right: 'row-reverse',
+  up: 'column',
+  down: 'column-reverse',
+};
+
+const MARGIN_ATRR_MAP = {
+  left: 'marginRight',
+  right: 'marginLeft',
+  up: 'marginBottom',
+  down: 'marginTop',
+};
+
+const ButtonWrapper = styled.div`
+  display: inline-flex;
+  flex-direction: ${props => FLEX_DIRECTION_MAP[props.direction]};
+  align-items: center;
   cursor: pointer;
   &:hover {
     transform: translate(0, -2px);
@@ -10,7 +27,7 @@ const ButtonSVG = styled(SVG)`
   &:active {
     transform: translate(0, 0);
   }
-`
+`;
 
 class TriangleButton extends React.Component {
   render() {
@@ -19,26 +36,28 @@ class TriangleButton extends React.Component {
       height = 40,
       color = '#aaa',
       direction = 'right',
+      children,
+      style,
     } = this.props;
     const padding = 3;
 
     const vertices = {
-      'up': [
+      up: [
         [padding, height - padding], // [x, y]
         [width - padding, height - padding],
         [width / 2, padding],
       ],
-      'down': [
+      down: [
         [padding, padding],
         [width - padding, padding],
         [width / 2, height - padding],
       ],
-      'left': [
+      left: [
         [width - padding, padding],
         [width - padding, height - padding],
         [padding, height / 2],
       ],
-      'right': [
+      right: [
         [padding, padding],
         [padding, height - padding],
         [width - padding, height / 2],
@@ -46,15 +65,21 @@ class TriangleButton extends React.Component {
     };
 
     return (
-      <ButtonSVG width={width} height={height}>
-        <Polygon
-          vertices={vertices[direction]}
-          options={{
-            fill: color,
-            fillWeight: 3,
-          }}
-        />
-      </ButtonSVG>
+      <ButtonWrapper direction={direction} style={style}>
+        <SVG
+          width={width}
+          height={height}
+          style={{[MARGIN_ATRR_MAP[direction]]: 10}}>
+          <Polygon
+            vertices={vertices[direction]}
+            options={{
+              fill: color,
+              fillWeight: 3,
+            }}
+          />
+        </SVG>
+        {children}
+      </ButtonWrapper>
     );
   }
 }
