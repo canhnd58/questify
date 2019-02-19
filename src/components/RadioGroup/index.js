dd71453f322ddd62a1a17020b34e5cdd8e466f72
Tung Teng <tung.nguyen@employmenthero.com>
2019-02-19 22:44:05 +0700

    Update RadioGroup: Wrap a div around all options

--- src/components/RadioGroup/index.js
+++ src/components/RadioGroup/index.js
@@ -6,18 +6,26 @@ import {SVG, Circle} from '../../core/ReactRough';
 
 const Wrapper = styled.div`
   display: ${props => (props.inline ? 'inline-flex' : 'flex')};
+  flex-direction: ${props => (props.inline ? 'row' : 'column')};
+`;
+
+const Option = styled.div`
+  display: flex;
   align-items: center;
   cursor: pointer;
   margin-right: 10px;
 `;
 
-const RadioGroup = ({options = [], activeRadio, setActiveRadio, inline}) =>
-  options.map((value, key) => (
-    <Wrapper key={key} onClick={() => setActiveRadio(key)} inline={inline}>
-      <RadioButton key={key} active={activeRadio === key} />
-      {value}
-    </Wrapper>
-  ));
+const RadioGroup = ({options = [], activeRadio, setActiveRadio, inline}) => (
+  <Wrapper inline={inline}>
+    {options.map((value, key) => (
+      <Option key={key} onClick={() => setActiveRadio(key)}>
+        <RadioButton key={key} active={activeRadio === key} />
+        {value}
+      </Option>
+    ))}
+  </Wrapper>
+);
 
 const enhance = compose(
   withState('activeRadio', 'setActiveRadio', null),
